a7248c3fe8b9f6b7b25815ad370222d2188cdb74
David Dias <daviddias.p@gmail.com>
2018-12-09 17:48:29 +0000

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm install -g n ipfs asciify speed-test wifi-password-cli standard npm-check-updates
+npm install -g n ipfs asciify speed-test wifi-password-cli standard npm-check-updates thanks
